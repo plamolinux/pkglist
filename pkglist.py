b63337b5a4e96883b79c43f5bdeb9be757f68cc8
KOJIMA Mitsuhiro <kojima@linet.gr.jp>
2015-01-26 22:42:38 +0900

    Remove unneeded lines
    Remove the old structure of replace_list.
    
    Signed-off-by: KOJIMA Mitsuhiro <kojima@linet.gr.jp>

--- pkglist.py
+++ pkglist.py
@@ -17,20 +17,6 @@ blockpkgs = ('aaa_base', 'hdsetup', 'etc', 'sysvinit', 'shadow', 'network_config
 '''
  __replaces: 改名，分割，集約されたパッケージを追跡するために，旧パッケージ名を新パッケージ名にマップする．
 ex:  'tamago' -> 'tamago_tsunagi', 'python' -> 'Python2', 'Python3' -> 'Python'
-分割されたパッケージを追跡するために、新パッケージ名はタプルにして一対多の対応を可能にしたが、
-カテゴリごとに新パッケージを表示する機能を追加したので一対多のマッピングは不要になったものの、
-新パッケージ名のリストはタプルのままなので，要素が一つだけでも末尾に","を付ける必要あり．
-
-旧仕様:
-replace_list = {'tamago':('20110401', ('tamago_tsunagi',)), 
-                'emacs':('24.2', ('emacs_mini', 'emacs_bin', 'emacs_lib', 'emacs')), 
-                'Python3':('3.3.2', ('Python',)), 
-                'python':('2.7.3', ('Python2',)), 
-                'pycups2':('1.9.70', ('py2cups',)), 
-                'pycurl2':('7.19.5',('py2curl',)),
-		'python_setuptools':('0.6c11',('python_setuptools', 'python2_setuptools')),
-                'xfwm4_themes':('4.6.0',('xfwm4_themes', 'xfce_theme_albatross', 'xfce_theme_greybird', 'xfce_theme_numix', 'xfce_theme_orion')),
-               }
 '''
 replace_list = {'tamago':'tamago_tsunagi', 
                 'Python3':'Python',
@@ -39,7 +25,6 @@ replace_list = {'tamago':'tamago_tsunagi',
                 'pycurl2':'py2curl'
                 }
 
-#allpkgs['__replaces'] = replace_list
 
 for arch in archdir:
     allpkgs= {}
@@ -60,7 +45,7 @@ for arch in archdir:
                
                 (base, vers, p_arch, tmp) = j.split("-")
                 (build, ext) = tmp.split(".") 
-                r_path = root.replace("/home/ftp/pub/Plamo-5.x/", "")
+                r_path = root.replace(basedir, "")
                 data_t = (vers, p_arch, build, ext, r_path)
                 allpkgs[base] = data_t
 
